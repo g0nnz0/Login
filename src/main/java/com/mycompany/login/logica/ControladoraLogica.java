ec3803711c9c1e6bca3b8fda9a3539b70849fb21
g0nnz0 <bonzai_rock@hotmail.com>
2023-09-27 23:37:57 -0300

    Creé en logica la ControladoraLogica la cual crea una instancia de la ControladoraPersistencia

---
+++ src/main/java/com/mycompany/login/logica/ControladoraLogica.java
@@ -0,0 +1,8 @@
+package com.mycompany.login.logica;
+
+import com.mycompany.login.persistencia.ControladoraPersistencia;
+
+public class ControladoraLogica {
+
+    ControladoraPersistencia controlPersis = new ControladoraPersistencia();
+}
